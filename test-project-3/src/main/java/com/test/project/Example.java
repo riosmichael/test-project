4e707b937fe5433d79f5f20e110d75a616604d6a
michael rios <me@michaelrios.com>
2019-04-07 22:14:52 +0800

    commit changes

---
+++ test-project-3/src/main/java/com/test/project/Example.java
@@ -0,0 +1,15 @@
+package com.test.project;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Example {
+
+	@RequestMapping("/")
+	String home() {
+		return "Hello World!";
+	}
+
+
+}
